6f9eb9c739d994a53a837a32345b9c2ef1feabca
tarlepp <tarmo.leppanen@protacon.com>
2016-09-27 21:59:11 +0300

    Changed color palette to "match" with http://www.poke.fi/

--- src/dependencies/dependencies.config.js
+++ src/dependencies/dependencies.config.js
@@ -9,7 +9,7 @@ export default ($mdThemingProvider) => {
   // Configure angular-material theme
   $mdThemingProvider
     .theme('default')
-    .primaryPalette('blue-grey')
-    .accentPalette('blue')
+    .primaryPalette('red')
+    .accentPalette('green')
   ;
 };
